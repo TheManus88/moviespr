dbcc8729dd54115f8aa03b011aa810bf254a83a0
manu <raccoonlabdev@gmail.com>
2024-05-02 19:22:41 -0600

    read env variables

--- src/main.js
+++ src/main.js
@@ -8,5 +8,5 @@ import AxiosPlugin from "./plugin/axios";
 const app = createApp(App);
 
 app.use(router);
-app.use(AxiosPlugin, { baseUrl: process.env.BASE_URL });
+app.use(AxiosPlugin, { baseUrl: import.meta.env.BASE_URL });
 app.mount("#app");
